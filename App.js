98da696887bdc744ef282eedf388b8b2feeab3bf
hasn-ab <67307736+hasn-ab@users.noreply.github.com>
2021-11-02 22:32:56 +0500

    setup common packages

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
-
+import 'react-native-gesture-handler';
 const App = () => {
   return <SafeAreaView></SafeAreaView>;
 };
