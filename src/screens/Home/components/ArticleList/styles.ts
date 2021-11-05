834705b1f5895ee093b8678aa0635ff61a35ccf6
hasn-ab <67307736+hasn-ab@users.noreply.github.com>
2021-11-06 01:14:42 +0500

    updated style for feed list

--- src/screens/Home/components/ArticleList/styles.ts
+++ src/screens/Home/components/ArticleList/styles.ts
@@ -1,7 +1,11 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {flex: 1, padding: 12, backgroundColor: '#dadada'},
+  container: {
+    flex: 1,
+    paddingHorizontal: 12,
+    backgroundColor: '#dadada',
+  },
   listContainer: {},
 });
 export default styles;
