69844f3bceff0d3acd5d1ebb2ec58789d7220312
hasn-ab <67307736+hasn-ab@users.noreply.github.com>
2021-11-06 01:13:46 +0500

    updated homeservice to dispatch api request

--- src/screens/Home/HomeService.tsx
+++ src/screens/Home/HomeService.tsx
@@ -1,9 +1,26 @@
 import React, {useEffect} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {useAppDispatch} from '@redux/hooks';
+import * as Actions from './redux/actionTypes';
+
+/**
+ * HomeService will be used for functional part of
+ * code like click handlers, dispatching redux actions
+ * It wraps UI of each screen
+ */
 const HomeService = ({children}: any) => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch({
+      type: Actions.GET_NEWS_FEED,
+    });
+  }, []);
+
   return children({
     /**
-  handlers, data, will go here
+    handlers, data, will be passed to UI here
+    UI will get them as props
    */
   });
 };
--- src/screens/Home/HomeUI.tsx
+++ src/screens/Home/HomeUI.tsx
@@ -3,6 +3,12 @@ import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
 import {HomeProps} from './propTypes';
 import Sections from './components/Sections';
 import ArticleList from './components/ArticleList';
+
+/**
+ * HomeUI will only render the UI
+ * all functional part of component will be passed
+ * down from service
+ */
 const HomeUI = (props: HomeProps) => {
   return (
     <SafeAreaView style={styles.container}>
